b942920d950f53ceedc96f15adc1621f1d92b11c
shoxrux <shoxruxnorqoziyev2004@gmail.com>
2023-08-04 07:27:00 +0500

    min item count

--- find08_min_count.py
+++ find08_min_count.py
@@ -5,4 +5,10 @@ def find_min_count(data):
         data: list of numbers
     returns: count of minimum numbers in the list
     """
-    return 0
+    a=sorted(data)
+    if a[0]==a[1]:
+        return 2
+    else:
+        return 1
+print(find_min_count([2,5,9,11,6,3]))
+print(find_min_count([0,-4,3,9,-2,-4]))
