1e8bdcf02a34c3f4e47a66c3dbeeab94c21b72eb
shoxrux <shoxruxnorqoziyev2004@gmail.com>
2023-08-04 07:27:25 +0500

    min index

--- find09_min_index.py
+++ find09_min_index.py
@@ -5,5 +5,8 @@ def find_min_index(data):
         data: list of numbers
     returns: index of minimum number in the list
     """
-    return 0
-
+    a=min(data)
+    b=data.index(a)
+    return b
+print(find_min_index([4,6,8,5,2,5]))
+print(find_min_index([12,5,2,7,9,1]))
